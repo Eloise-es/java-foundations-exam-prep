55b9b316ba32f42037ed53ed4e4c8f33fa764b50
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-11-14 18:02:00 +0100

    Package and compiling practice

---
+++ 2-Basic-Java-Elements/compilation-2/A.java
@@ -0,0 +1,8 @@
+package p1;
+import p2.B;
+public class A{
+    public static void main(String[] args) {
+        System.out.println("Making new B");
+    }
+    B b = new B();
+}
---
+++ 2-Basic-Java-Elements/compilation/Account.java
@@ -0,0 +1,8 @@
+package accounting;
+public class Account {
+    private String accountNumber;
+    public static void main(String[] args) {
+        System.out.println("123 testing...");
+    }
+
+}
