23d2eb536314131da0a2605c9136a07f8acfce48
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-11-22 10:50:57 +0100

    More tests with types

--- 1-Java-Basics/MyFirstJavaProgram.java
+++ 1-Java-Basics/MyFirstJavaProgram.java
@@ -1,5 +1,12 @@
 class MyFirstJavaProgram {
     public static void main(String[] args) {
+        int i = 10;
+        double d = 3.4;
+        double d2 = 10.11;
         System.out.println("Hello world!");
+        System.out.println(d / 0);
+        System.out.println(d2 % 0);
+        System.out.println(Double.POSITIVE_INFINITY);
+        System.out.println("______________");;;;;;;
     }
 }
